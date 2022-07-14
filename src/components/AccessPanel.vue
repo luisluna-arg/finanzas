b1c248aabfbc3f1275a90d12ae1d14205bb8f32b
lluna-patagonian <70983181+lluna-patagonian@users.noreply.github.com>
2022-07-14 14:30:42 -0300

    InitialCommit

---
+++ src/components/AccessPanel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="panel">
+        <label>{{PanelName}}</label>
+        <button>Importar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccessPanel',
+  props: ['PanelName'],
+  components: {
+  },
+  data() {
+      return {
+          
+      }
+  },
+  methods: {
+    
+  },
+  mounted () {
+    this.PanelName = "Fondos";
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <AccessPanel :numberOfUsers="Fondos"></AccessPanel>
+  </div>
+</template>
+
+<script>
+import AccessPanel from './AccessPanel.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    AccessPanel
+  },
+  data() {
+      return {
+          
+      }
+  },
+  methods: {
+    
+  },
+  mounted () {
+    this.getAllUsers();
+  }
+}
+</script>
