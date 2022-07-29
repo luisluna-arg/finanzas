a3781b82cd0a58993740587e0a44d54650a196f9
lluna-patagonian <70983181+lluna-patagonian@users.noreply.github.com>
2022-07-29 15:17:42 -0300

    Partial Crud popup component implementation

--- FinanceFront/src/components/FondosComponent.vue
+++ FinanceFront/src/components/FondosComponent.vue
@@ -1,36 +1,45 @@
 <template>
-    <div>
-        <h1>Fondos</h1>    
-        <ImportButtonComponent :UploadType="FundsUploadType"></ImportButtonComponent>
-        <FondosChartComponent></FondosChartComponent>
-        <FondosGridComponent></FondosGridComponent>
-        <FondosTotalComponent></FondosTotalComponent>
-    </div>
+  <div>
+    <h1>Fondos</h1>
+    <ImportButtonComponent :UploadType="FundsUploadType"></ImportButtonComponent>
+    <!-- <FondosChartComponent></FondosChartComponent> -->
+    <FondosGridComponent></FondosGridComponent>
+    <FondosTotalComponent></FondosTotalComponent>
+    <CRUDPopUp :EditorSettings="CRUDPopUpSettings" />
+  </div>
 </template>
 
 <style>
-    h1 {
-        color:green;
-    }
+h1 {
+  color: green;
+}
 </style>
 
 <script>
 // @ is an alias to /src
 import FondosGridComponent from '@/components/fondos/FondosGridComponent.vue'
 import FondosTotalComponent from '@/components/fondos/FondosTotalComponent.vue'
-import FondosChartComponent from '@/components/fondos/FondosChartComponent.vue'
+// import FondosChartComponent from '@/components/fondos/FondosChartComponent.vue'
 import ImportButtonComponent from '@/components/utils/ImportButtonComponent.vue'
 import { UploadTypes } from '@/utils/commons'
+import CRUDPopUp from '@/components/utils/CRUDPopUp.vue'
 
 export default {
   components: {
     FondosGridComponent,
     FondosTotalComponent,
-    FondosChartComponent,
-    ImportButtonComponent
+    // FondosChartComponent,
+    ImportButtonComponent,
+    CRUDPopUp
   },
   data() {
-    return { FundsUploadType: UploadTypes.Funds }
+    return {
+      FundsUploadType: UploadTypes.Funds,
+      CRUDPopUpSettings: [
+        { id: "name", type: "TextInput", label: "Nombre" },
+        { id: "lastname", type: "TextInput", label: "Apellido" }
+      ]
+    }
   }
 }
 </script>
---
+++ FinanceFront/src/components/utils/CRUDPopUpInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <label for:={{ settings.id }}>{{ settings.label }}</label>
+        <input id:={{ settings.id }} type="text" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        settings: Object
+    },
+    data: function () {
+        console.log(this.props);
+        return {
+            items: []
+        };
+    }
+}
+</script>
+
+<style>
+</style>
