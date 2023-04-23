41afa33619d850aa1642dd7028db41e28995fac6
Luis Luna <luismluna87@gmail.com>
2023-04-23 04:43:45 -0300

    Added new Api handler

---
+++ FinanceFront/src/api/movementsApi.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { ApiUrls, APIs } from './../utils/commons';
+
+const methods = {
+    get: "GET",
+    post: "POST",
+    del: "DELETE",
+    put: "PUT"
+}
+
+const makeCall = (method = methods.get, url, successCallback, catchCallback, finallyCallback) => {
+    axios({
+        method: method,
+        url: url
+      })
+      .then(successCallback)
+      .catch(catchCallback)
+      .finally(finallyCallback);
+}
+
+const totals = (successCallback, catchCallback, finallyCallback) => {
+    makeCall(methods.get, ApiUrls[APIs.Movement].totals, successCallback, catchCallback, finallyCallback);
+}
+
+const movementsApi = {
+    totals,
+};
+
+export default movementsApi;
