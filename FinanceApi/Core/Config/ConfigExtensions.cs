f07eee2a350b60bd8a51960432e9d3232a855503
Luis Luna <luismluna87@gmail.com>
2023-07-03 03:47:43 -0300

    Removed unused Dto factories

--- FinanceApi/Core/Config/ConfigExtensions.cs
+++ FinanceApi/Core/Config/ConfigExtensions.cs
@@ -1,13 +1,9 @@
-using FinanceApi.Application.Dtos.Factories;
-
 namespace FinanceApi.Core.Config;
 
 public static class ConfigExtensions
 {
     public static void MainServices(this IServiceCollection services)
     {
-        services.AddSingleton<IMovementDtoFactory, MovementDtoFactory>();
-
         services.AddMediatR(o => o.RegisterServicesFromAssembly(typeof(Program).Assembly));
     }
 }
