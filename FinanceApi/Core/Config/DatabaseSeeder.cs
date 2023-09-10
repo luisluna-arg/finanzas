536adc042d6a13877848bc81d027ee2422cda752
Luis Luna <luismluna87@gmail.com>
2023-09-10 19:16:15 -0300

    Fixed database seeder

--- FinanceApi/Core/Config/DatabaseSeeder.cs
+++ FinanceApi/Core/Config/DatabaseSeeder.cs
@@ -33,46 +33,84 @@ public class DatabaseSeeder : IHostedService
 
     private async Task SeedDefaultCurrencies(FinanceDbContext dbContext)
     {
-        if (dbContext.Currency.Any()) return;
+        var currencies = await dbContext.Currency.ToArrayAsync();
 
-        await dbContext.Currency.AddRangeAsync(
-            new Currency { Name = CurrencyNames.Peso, ShortName = CurrencyNames.Peso, },
-            new Currency { Name = CurrencyNames.Dollar, ShortName = CurrencyNames.Dollar, });
+        var newCurrencies = new List<Currency>();
 
-        await dbContext.SaveChangesAsync();
+        Action<string, string> collectCurrency = (currencyName, currencyShortName) =>
+        {
+            if (!currencies.Any(o => o.Name.Equals(currencyName, StringComparison.InvariantCulture)))
+            {
+                newCurrencies.Add(new Currency { Name = currencyName, ShortName = currencyShortName });
+            }
+        };
+
+        collectCurrency(CurrencyNames.Peso, CurrencyNames.Peso);
+        collectCurrency(CurrencyNames.Dollar, CurrencyNames.Dollar);
+
+        if (newCurrencies.Any())
+        {
+            await dbContext.AddRangeAsync(newCurrencies);
+            await dbContext.SaveChangesAsync();
+        }
     }
 
     private async Task SeedAppModules(FinanceDbContext dbContext)
     {
-        if (dbContext.AppModule.Any()) return;
-
-        var currencyPeso = await dbContext.Currency.FirstOrDefaultAsync(x => x.Name == CurrencyNames.Peso);
-        if (currencyPeso == null) throw new SystemException($"Fatal error while seeding App database: Currency {CurrencyNames.Peso}");
-
-        var currencyDollar = await dbContext.Currency.FirstOrDefaultAsync(x => x.Name == CurrencyNames.Dollar);
-        if (currencyDollar == null) throw new SystemException($"Fatal error while seeding App database: Currency {CurrencyNames.Dollar}");
+        var currencies = await dbContext.Currency.ToArrayAsync();
+        var appModules = await dbContext.AppModule.ToArrayAsync();
 
         var now = DateTime.Now.ToUniversalTime();
 
-        await dbContext.AppModule.AddRangeAsync(new List<AppModule>
-            {
-                new AppModule { Name = AppModuleNames.Funds, CreatedAt = now, Currency = currencyPeso },
-                new AppModule { Name = AppModuleNames.DollarFunds, CreatedAt = now, Currency = currencyDollar },
-                new AppModule { Name = AppModuleNames.IOLInvestments, CreatedAt = now, Currency = currencyPeso },
-            });
+        var newModules = new List<AppModule>();
 
-        await dbContext.SaveChangesAsync();
+        Action<string, string> collectModule = (moduleName, currencyName) =>
+        {
+            if (!appModules.Any(o => o.Name.Equals(moduleName, StringComparison.InvariantCulture)))
+            {
+                var currencyPeso = currencies.FirstOrDefault(x => x.Name == currencyName);
+                if (currencyPeso == null) throw new SystemException($"Fatal error while seeding App database: Currency not found {currencyName}");
+
+                newModules.Add(new AppModule { Name = moduleName, CreatedAt = now, Currency = currencyPeso });
+            }
+        };
+
+        collectModule(AppModuleNames.Funds, CurrencyNames.Peso);
+
+        collectModule(AppModuleNames.DollarFunds, CurrencyNames.Dollar);
+
+        collectModule(AppModuleNames.IOLInvestments, CurrencyNames.Peso);
+
+        if (newModules.Any())
+        {
+            await dbContext.AddRangeAsync(newModules);
+            await dbContext.SaveChangesAsync();
+        }
     }
 
     private async Task SeedInvestmentAssetIOLTypes(FinanceDbContext dbContext)
     {
-        if (dbContext.IOLInvestmentAssetTypes.Any()) return;
+        var investmentAssetTypes = await dbContext.IOLInvestmentAssetTypes.ToArrayAsync();
 
-        await dbContext.IOLInvestmentAssetTypes.AddRangeAsync(
-            EnumHelper.GetEnumMembers<IOLInvestmentAssetTypeEnum>()
-            .Select(x => new IOLInvestmentAssetType() { Id = (ushort)x, Name = x.ToString() }));
+        var enumValueInstances = EnumHelper.GetEnumMembers<IOLInvestmentAssetTypeEnum>().ToList();
 
-        await dbContext.SaveChangesAsync();
+        var newInvestmentAssetTypes = new List<IOLInvestmentAssetType>();
+
+        Action<ushort, string> collectInvestmentAssetTypes = (id, name) =>
+        {
+            if (!investmentAssetTypes.Any(o => o.Id == id))
+            {
+                newInvestmentAssetTypes.Add(new IOLInvestmentAssetType() { Id = id, Name = name });
+            }
+        };
+
+        enumValueInstances.ForEach(o => collectInvestmentAssetTypes((ushort)o, o.ToString()));
+
+        if (newInvestmentAssetTypes.Any())
+        {
+            await dbContext.AddRangeAsync(newInvestmentAssetTypes);
+            await dbContext.SaveChangesAsync();
+        }
     }
 
     public static class CurrencyNames
