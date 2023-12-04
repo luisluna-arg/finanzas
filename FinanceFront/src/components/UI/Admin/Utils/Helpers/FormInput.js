c74573c3ef415d3bd9db35e26ded18f0d56b0d2f
Luis Luna <luismluna87@gmail.com>
2023-12-03 23:14:57 -0300

    Front | Reduced FormInput helper

--- FinanceFront/src/components/UI/Admin/Utils/Helpers/FormInput.js
+++ FinanceFront/src/components/UI/Admin/Utils/Helpers/FormInput.js
@@ -1,41 +1,9 @@
-// MyClass.js
 export default class FormInput {
-  constructor(id, type, label, placeholder = null) {
-    this._id = id;
-    this._type = type;
-    this._label = label;
-    this._placeholder = placeholder;
-  }
-
-  get id() {
-    return this._value;
-  }
-
-  set id(value) {
-    this._id = value;
-  }
-
-  get type() {
-    return this._value;
-  }
-
-  set type(value) {
-    this._type = value;
-  }
-
-  get label() {
-    return this._value;
-  }
-
-  set label(value) {
-    this._label = value;
-  }
-
-  get placeholder() {
-    return this._value;
-  }
-
-  set placeholder(value) {
-    this._placeholder = value;
+  constructor(id, type, label, placeholder = null, visible = true) {
+    this.id = id;
+    this.type = type;
+    this.label = label;
+    this.placeholder = placeholder;
+    this.visible = visible;
   }
 }
