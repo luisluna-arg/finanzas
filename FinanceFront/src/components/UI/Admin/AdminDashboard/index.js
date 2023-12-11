74cdd8a5bec8268fb2c69152d1e9c6377ffae143
Luis Luna <luismluna87@gmail.com>
2023-12-10 21:19:33 -0300

    Front | Removed unuseful comment

--- FinanceFront/src/components/UI/Admin/AdminDashboard/index.js
+++ FinanceFront/src/components/UI/Admin/AdminDashboard/index.js
@@ -43,11 +43,6 @@ const AdminDashboard = () => {
                   </Nav.Link>
                 </Nav.Item>
               ))}
-              {/* <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                  Disabled
-                </Nav.Link>
-              </Nav.Item> */}
             </Nav>
           </div>
         </Col>
