9a4139db99f16aca3dd34ec3f9a5f813b8a02e29
Luis Luna <luismluna87@gmail.com>
2023-12-17 18:22:38 -0300

    Front | Added picker value mapper

--- FinanceFront/src/components/utils/Picker/index.js
+++ FinanceFront/src/components/utils/Picker/index.js
@@ -26,11 +26,16 @@ function Picker({ id, url, mapper, onChange, onFetch }) {
 
   return (
     <select id={id} className="form-select" onChange={onPickerChange} value={selectedValue}>
-      {data.map((record) => (
-        <option key={record[mapper.id]} value={record[mapper.id]}>
-          {record[mapper.label]}
-        </option>
-      ))}
+      {data.map((record) => {
+        const id = typeof mapper.id === "function" ? mapper.id(record) : record[mapper.id];
+        const label = typeof mapper.label === "function" ? mapper.label(record) : record[mapper.label];
+
+        return ((
+          <option key={id} value={id}>
+            {label}
+          </option>
+        ));
+      })}
     </select>
   );
 }
