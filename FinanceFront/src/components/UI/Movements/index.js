956ade763503f0acb49afad49e20e30c36fbc586
Luis Luna <luismluna87@gmail.com>
2023-12-24 19:23:15 -0300

    Front | Fixes in Movements UI

--- FinanceFront/src/components/UI/Movements/index.js
+++ FinanceFront/src/components/UI/Movements/index.js
@@ -4,41 +4,66 @@ import urls from "../../../routing/urls";
 import Uploader from "../../utils/Uploader";
 import Picker from "../../utils/Picker";
 import PaginatedTable from "../../utils/PaginatedTable";
+import { InputControlTypes } from "../../utils/InputControl";
 
 function Movements() {
-  const [selectedBankId, setSelectedBankId] = useState("");
-  const [selectedAppModuleId, setSelectedAppModuleId] = useState("");
-  const [fundsUploadEndpoint, setFundsUploadEndpoint] = useState(`${urls.funds.upload}`);
-  const [movementsEndpoint, setMovementsEndpoint] = useState(``);
+  const AppModuleTypeEnumFundsId = 1;
+
+  const [selectedBankId, setSelectedBankId] = useState(null);
+  const [selectedAppModuleId, setSelectedAppModuleId] = useState(null);
+  const [fundsUploadEndpoint, setFundsUploadEndpoint] = useState(null);
+  const [movementsEndpoint, setMovementsEndpoint] = useState(null);
+  const [appModulesEndpoint, setAppModulesEndpoint] = useState(`${urls.appModules.endpoint}?AppModuleType=${AppModuleTypeEnumFundsId}`);
 
   const refreshEndpoints = () => {
-    setFundsUploadEndpoint(`${urls.movements.upload}?DateKind=Local&AppModuleId=${selectedAppModuleId}&BankId=${selectedBankId}`);
-    setMovementsEndpoint(`${urls.movements.paginated}?AppModuleId=${selectedAppModuleId}&BankId=${selectedBankId}`);
+    if (selectedAppModuleId && selectedBankId) {
+      setFundsUploadEndpoint(`${urls.movements.upload}?DateKind=Local&AppModuleId=${selectedAppModuleId}&BankId=${selectedBankId}`);
+      setMovementsEndpoint(`${urls.movements.paginated}?AppModuleId=${selectedAppModuleId}&BankId=${selectedBankId}`);
+    }
   }
 
   const onBankPickerChange = (picker) => {
     setSelectedBankId(picker.value);
+    refreshEndpoints();
   };
 
   const onBankPickerFetch = (data) => {
     setSelectedBankId(data[0].id);
+    refreshEndpoints();
   };
 
   const onAppModulePickerChange = (picker) => {
     setSelectedAppModuleId(picker.value);
+    refreshEndpoints();
   };
 
   const onAppModulePickerFetch = (data) => {
     setSelectedAppModuleId(data[0].id);
+    refreshEndpoints();
   };
 
   const onFetchFundsTable = (data) => {
-    
+
   }
 
   useEffect(() => {
     refreshEndpoints();
-  }, [selectedBankId, selectedAppModuleId]);
+  }, [
+    selectedBankId,
+    selectedAppModuleId,
+  ]);
+
+  console.log({
+    selectedBankId,
+    selectedAppModuleId,
+    fundsUploadEndpoint,
+    movementsEndpoint,
+    appModulesEndpoint,
+  });
+
+  let enabled = fundsUploadEndpoint
+    && movementsEndpoint
+    && appModulesEndpoint;
 
   return (
     <>
@@ -55,53 +80,70 @@ function Movements() {
           <div className="col-3">
             <Picker
               id={"module-picker"}
-              url={urls.appModules.endpoint}
+              url={appModulesEndpoint}
               mapper={{ id: "id", label: "name" }}
               onChange={onAppModulePickerChange}
               onFetch={onAppModulePickerFetch}
             />
           </div>
         </div>
-        <hr className="py-1" />
-        <Uploader url={fundsUploadEndpoint} extensions={[".xls", ".xlsx"]} />
-        <hr className="py-1" />
-        <PaginatedTable
-          name={"funds-table"}
-          url={movementsEndpoint}
-          onFetch={onFetchFundsTable}
-          columns={[
-            {
-              id: "timeStamp",
-              label: "Fecha",
-              type: "datetime"
-            },
-            {
-              id: "concept1",
-              label: "Concepto 1",
-            },
-            {
-              id: "concept2",
-              label: "Concepto 2",
-            },
-            {
-              id: "amount",
-              label: "Monto",
-              headerClass: "text-end",
-              class: "text-end",
-              mapper: (field) => parseFloat(field.value.toFixed(2)),
-              conditionalClass: {
-                class: "text-success fw-bold",
-                eval: (field) => field.value > 0
+        {!enabled && <div>Cargando...</div>}
+        {enabled && <div>
+          <hr className="py-1" />
+          <Uploader url={fundsUploadEndpoint} extensions={[".xls", ".xlsx"]} />
+          <hr className="py-1" />
+          <PaginatedTable
+            name={"funds-table"}
+            url={movementsEndpoint}
+            onFetch={onFetchFundsTable}
+            columns={[
+              {
+                id: "timeStamp",
+                label: "Fecha",
+                placeholder: "Fecha",
+                type: InputControlTypes.DateTime,
+                editable: true,
+                datetime: {
+                  timeFormat: "HH:mm",
+                  timeIntervals: 15,
+                  dateFormat: "DD/MM/YYYY HH:mm",
+                  placeholder: "Seleccionar fecha",
+                },
+                header: {
+                  style: {
+                    width: "160px"
+                  }
+                }
+              },
+              {
+                id: "concept1",
+                label: "Concepto 1",
+              },
+              {
+                id: "concept2",
+                label: "Concepto 2",
+              },
+              {
+                id: "amount",
+                label: "Monto",
+                headerClass: "text-end",
+                class: "text-end",
+                mapper: (field) => parseFloat(field.value.toFixed(2)),
+                conditionalClass: {
+                  class: "text-success fw-bold",
+                  eval: (field) => field.value > 0
+                }
+              },
+              {
+                id: "total",
+                label: "Total",
+                headerClass: "text-end",
+                class: "text-end",
+                mapper: (field) => parseFloat(field.value.toFixed(2)),
               }
-            },
-            {
-              id: "total",
-              label: "Total",
-              headerClass: "text-end",
-              class: "text-end",
-              mapper: (field) => parseFloat(field.value.toFixed(2)),
-            }
-          ]} />
+            ]} />
+        </div>
+        }
       </div>
     </>
   );
--- FinanceFront/src/components/utils/Picker/index.js
+++ FinanceFront/src/components/utils/Picker/index.js
@@ -4,11 +4,23 @@ function Picker({ id, url, mapper, onChange, onFetch }) {
   const [data, setData] = useState([]);
   const [selectedValue, setSelectedValue] = useState("");
 
+  const getRecordId = record => {
+    return typeof mapper.id === "function" ? mapper.id(record) : record[mapper.id];
+  }
+
+  // const getPicker = () => {
+  //   return document.getElementById(id);
+  // }
+
   const fetchData = async () => {
     try {
       let response = await fetch(url);
       let data = await response.json();
       setData(data);
+      // if (data.length > 0)
+      // {
+      //   getPicker().value = getRecordId(data[0]);
+      // }
       onFetch && onFetch(data)
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -26,12 +38,12 @@ function Picker({ id, url, mapper, onChange, onFetch }) {
 
   return (
     <select id={id} className="form-select" onChange={onPickerChange} value={selectedValue}>
-      {data.map((record) => {
-        const id = typeof mapper.id === "function" ? mapper.id(record) : record[mapper.id];
+      {data.map((record, index) => {
+        const id = getRecordId(record);
         const label = typeof mapper.label === "function" ? mapper.label(record) : record[mapper.label];
 
         return ((
-          <option key={id} value={id}>
+          <option key={id} value={id} defaultValue={id}>
             {label}
           </option>
         ));
