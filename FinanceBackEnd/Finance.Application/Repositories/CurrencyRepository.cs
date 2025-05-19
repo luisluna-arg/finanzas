d03228fa174c1553321cb9f7ac8e8bc707297e16
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2025-05-19 03:09:31 -0300

    Updated IOLExcelHelper and Currency repositories

--- FinanceBackEnd/Finance.Application/Repositories/CurrencyRepository.cs
+++ FinanceBackEnd/Finance.Application/Repositories/CurrencyRepository.cs
@@ -1,6 +1,7 @@
 using Finance.Domain.Models;
 using Finance.Application.Repositories.Base;
 using Finance.Persistance;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Application.Repositories;
 
@@ -9,5 +10,25 @@ public class CurrencyRepository : BaseRepository<Currency, Guid>
     public CurrencyRepository(FinanceDbContext dbContext)
         : base(dbContext)
     {
+    }
+
+    public override async Task<Currency?> GetByAsync(string searchCriteria, object searchValue, CancellationToken cancellationToken)
+    {
+        if (searchCriteria == "Symbol")
+        {
+            var symbols = searchValue as IEnumerable<string>;
+
+            if (symbols?.Count() > 0)
+            {
+                return await DbContext.Currency
+                    .Include(c => c.Symbols)
+                    .Where(c => c.Symbols.Any(s => symbols.Contains(s.Symbol)))
+                    .FirstAsync(c => c.Symbols.Any(s => symbols.Contains(s.Symbol)), cancellationToken);
+            }
+
+            return null;
+        }
+
+        return await base.GetByAsync(searchCriteria, searchValue, cancellationToken);
     }
 }
---
+++ FinanceBackEnd/Finance.Application/Repositories/CurrencySymbolRepository.cs
@@ -0,0 +1,13 @@
+using Finance.Domain.Models;
+using Finance.Application.Repositories.Base;
+using Finance.Persistance;
+
+namespace Finance.Application.Repositories;
+
+public class CurrencySymbolRepository : BaseRepository<CurrencySymbol, ushort>
+{
+    public CurrencySymbolRepository(FinanceDbContext dbContext)
+        : base(dbContext)
+    {
+    }
+}
