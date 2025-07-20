7780aaaf879cc84d6341d39f67174e9139efbde5
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2025-07-20 09:01:55 -0300

    Backend: Application new users and resources queries

---
+++ FinanceBackEnd/Finance.Application/Queries/Users/GetUserBySourceIdsQuery.cs
@@ -0,0 +1,42 @@
+using CQRSDispatch;
+using CQRSDispatch.Interfaces;
+using Finance.Domain.Models;
+using Finance.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Application.Queries.Users;
+
+public class GetUserBySourceIdsQuery : IQuery<User?>
+{
+    public string[] SourceIds { get; }
+
+    public GetUserBySourceIdsQuery(string[] sourceIds)
+    {
+        SourceIds = sourceIds;
+    }
+}
+
+public class GetUserBySourceIdsQueryHandler : IQueryHandler<GetUserBySourceIdsQuery, User?>
+{
+    public FinanceDbContext DbContext { get; }
+
+    public GetUserBySourceIdsQueryHandler(FinanceDbContext dbContext)
+    {
+        DbContext = dbContext;
+    }
+
+    public async Task<DataResult<User?>> ExecuteAsync(GetUserBySourceIdsQuery request, CancellationToken cancellationToken = default)
+    {
+        var user = await DbContext.User
+            .Include(u => u.Roles)
+            .Include(u => u.Identities)
+            .FirstOrDefaultAsync(u => u.Identities.Any(i => request.SourceIds.Contains(i.SourceId)), cancellationToken);
+
+        if (user == null)
+        {
+            return DataResult<User?>.Failure("User not found with the provided source IDs.");
+        }
+
+        return DataResult<User?>.Success(user);
+    }
+}
