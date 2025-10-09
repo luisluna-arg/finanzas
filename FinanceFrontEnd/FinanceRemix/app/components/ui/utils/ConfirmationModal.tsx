98d07ca967d49b62c367c83e6fadf84042f30f3a
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2025-10-09 17:34:39 -0300

    Frontend: Remix CI issues

--- FinanceFrontEnd/FinanceRemix/app/components/ui/utils/ConfirmationModal.tsx
+++ FinanceFrontEnd/FinanceRemix/app/components/ui/utils/ConfirmationModal.tsx
@@ -1,58 +1,62 @@
 import React from "react";
 import { Form } from "@/components/ui/utils/Form";
 import {
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalTitle,
-  ModalFooter,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalTitle,
+    ModalFooter,
 } from "@/components/ui/utils/Modal";
 import ActionButton from "@/components/ui/utils/ActionButton";
 
 interface ConfirmationModalProps {
-  text: React.ReactNode;
-  title?: string;
-  show: boolean;
-  handleAccept: () => void;
-  handleCancel: () => void;
+    text: React.ReactNode;
+    title?: string;
+    show: boolean;
+    handleAccept: () => void;
+    handleCancel: () => void;
 }
 
 const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
-  text,
-  title,
-  show,
-  handleAccept,
-  handleCancel,
+    text,
+    title,
+    show,
+    handleAccept,
+    handleCancel,
 }) => {
-  return (
-    <Modal show={show} onHide={handleCancel}>
-      {title && (
-        <ModalHeader closeButton>
-          <ModalTitle>{title}</ModalTitle>
-        </ModalHeader>
-      )}
-      <ModalBody>
-        <Form
-          onSubmit={(event) => {
-            event.preventDefault();
-          }}
-          onReset={(event) => {
-            event.preventDefault();
-          }}
-        >
-          {text}
-        </Form>
-      </ModalBody>
-      <ModalFooter>
-        <ActionButton text="Aceptar" variant="success" action={handleAccept} />
-        <ActionButton
-          text="Cancelar"
-          variant="secondary"
-          action={handleCancel}
-        />
-      </ModalFooter>
-    </Modal>
-  );
+    return (
+        <Modal show={show} onHide={handleCancel}>
+            {title && (
+                <ModalHeader closeButton>
+                    <ModalTitle>{title}</ModalTitle>
+                </ModalHeader>
+            )}
+            <ModalBody>
+                <Form
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                    }}
+                    onReset={(event) => {
+                        event.preventDefault();
+                    }}
+                >
+                    {text}
+                </Form>
+            </ModalBody>
+            <ModalFooter>
+                <ActionButton
+                    text="Aceptar"
+                    variant="default"
+                    action={handleAccept}
+                />
+                <ActionButton
+                    text="Cancelar"
+                    variant="secondary"
+                    action={handleCancel}
+                />
+            </ModalFooter>
+        </Modal>
+    );
 };
 
 export default ConfirmationModal;
