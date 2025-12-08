b2f6e45fa4c6e76f18328bfdd35f53021e43b333
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2025-12-07 23:10:05 -0300

    Removing deprecated FinanceFront project (#20)

--- FinanceFrontEnd/FinanceFront/src/utils/common.js
+++
@@ -1,7 +0,0 @@
-const CommonUtils = {
-    Params: (obj) => Object.entries(obj)
-        .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-        .join('&')
-};
-
-export default CommonUtils;
