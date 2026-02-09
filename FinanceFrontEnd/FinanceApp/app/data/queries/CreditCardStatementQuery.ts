7aab9381c320c25fecd3979387b08d62753fbd81
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2026-02-09 02:27:10 -0300

    Added Latest Statements query to backend client

--- FinanceFrontEnd/FinanceApp/app/data/queries/CreditCardStatementQuery.ts
+++ FinanceFrontEnd/FinanceApp/app/data/queries/CreditCardStatementQuery.ts
@@ -1,11 +1,29 @@
+import axios from "axios";
 import urls from "@/utils/urls";
 import { Agent } from "https";
 import { BaseQuery } from "../base/BaseQuery";
+import serverLogger from "@/utils/logger.server";
 
 export class CreditCardStatementQuery extends BaseQuery {
   constructor(httpsAgent: Agent, accessToken: string) {
     super(httpsAgent, accessToken, {
       get: urls.creditCardStatements.endpoint,
     });
+  }
+
+  async getLatest() {
+    try {
+      const config = {
+        httpsAgent: this.httpsAgent,
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+      };
+      const response = await axios.get(urls.creditCardStatements.latest, config);
+      return response.data;
+    } catch (error) {
+      serverLogger.error("Error fetching latest statements:", error);
+      throw error;
+    }
   }
 }
