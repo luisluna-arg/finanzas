ef8d79be2befc194c5acfbeb45eb639c4faff879
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2026-02-24 21:24:18 -0300

    FrontEnd: Fixing title reference at root

--- FinanceFrontEnd/FinanceApp/app/root.tsx
+++ FinanceFrontEnd/FinanceApp/app/root.tsx
@@ -107,9 +107,7 @@ export function ErrorBoundary() {
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>
-            {error.status} {error.statusText}
-          </title>
+          <title>{(`${error.status ?? ''} ${error.statusText ?? ''}`).trim()}</title>
           <Meta />
           <Links />
         </head>
